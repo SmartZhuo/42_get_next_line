20fc8174f38183799b766ed863996893b36f732c
Zhuo Wu <zhuwu@3-D-8.42heilbronn.de>
2025-11-08 20:01:05 +0100

    1108

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,43 @@
 /*   By: zhuwu <zhuwu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 14:11:57 by zhuwu             #+#    #+#             */
-/*   Updated: 2025/10/26 14:16:18 by zhuwu            ###   ########.fr       */
+/*   Updated: 2025/11/08 19:55:07 by zhuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "getopt.h"
+# include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	static char	*left_str;
+	char	*buf;
+	int		bytes;
+	char	*flag;
+	
+	left_str = NULL;
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (fd < 0|| BUFFER_SIZE <= 0)
+		return (NULL);
+	while (!ft_strchr(left_str, '\n'))
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		else if (bytes == 0)
+			break;
+		left_str = ft_strjoin(left_str, buf);
+	}
+	if (!left_str)
+	{
+		
+	}
 
+}
+	
+	
+	
 	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: zhuwu <zhuwu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 14:12:00 by zhuwu             #+#    #+#             */
-/*   Updated: 2025/10/26 14:14:13 by zhuwu            ###   ########.fr       */
+/*   Updated: 2025/11/08 15:52:43 by zhuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		GET_NEXT_LINE_H
 # define	GET_NEXT_LINE_H
+# define	BUFFER_SIZE
+
+#include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 
