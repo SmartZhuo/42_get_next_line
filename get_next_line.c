2f4a48a3402fa6530931ee114e4474ee2a2b7303
Zhuo <zhwu1988@gmail.com>
2025-11-21 17:48:50 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -10,10 +10,116 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "getopt.h"
+#include "get_next_line.h"
+
+// join and free
+char	*ft_free(char *buffer, char *buf)
+{
+	char	*temp;
+
+	temp = ft_strjoin(buffer, buf);
+	free(buffer);
+	return (temp);
+}
+
+char  *ft_read(int fd, char *res)
+{
+    char *buffer;
+    int flag;
+    char *tmp;
+
+    if(!res)
+      res = ft_calloc(1, 1);
+    buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+    flag = 1;
+    while(flag > 0)
+    {
+        flag = read(fd, buffer, BUFFER_SIZE);
+        if (flag == -1)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        buffer[flag] = '\0';
+        res = ft_free(res, buffer);
+        if (ft_strchr(buffer, '\n'))
+            break;
+    }
+    free(buffer);
+    return (res);
+}
+
+char *ft_get_line(char *buffer)
+{
+    char *line;
+    size_t i;
+
+    if(!buffer)
+        return (NULL);
+    if(buffer[0] == '\0')
+        return (NULL);
+    i = 0;
+    while(buffer[i] != '\0' && buffer[i] != '\n')
+        i++;
+    line = ft_calloc(i+ 2, sizeof(char));
+    i = 0;
+    while(buffer[i] != '\0' && buffer[i] != '\n')
+    {
+        line[i] = buffer[i];
+        i++;
+    }
+    if (buffer[i] == '\n')
+        line[i++] = '\n';
+    line[i] = '\0';   
+    return (line);
+}
+
+char *ft_new_buffer(char *buffer)
+{
+    char *new_buffer;
+    size_t i;
+    size_t j;
+
+    if(!buffer)
+        return (NULL);
+    if(buffer[0] == '\0')
+        return (NULL);
+
+    i = 0;
+    while(buffer[i] != '\0' && buffer[i] != '\n')
+        i++;
+    if(buffer[i] == '\0')
+    {
+        free(buffer);
+        return (NULL);
+    }
+    i++;
+    new_buffer = (char *)ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
+    if (!new_buffer)
+        return (NULL);
+    j = 0;
+    while(buffer[i] != '\0')
+        new_buffer[j++] = buffer[i++];   
+    new_buffer[j] = '\0';
+    free(buffer);
+    return (new_buffer);
+}
+
 
 char	*get_next_line(int fd)
 {
-
-	
+    char        *line;
+    static char *buffer; 
+    
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+        return (NULL);
+    //读取文件，把内容补到buffer中
+    buffer = ft_read(fd, buffer);
+    if (!buffer)
+        return (NULL);
+    // 从buffer中读取一行 包括/0
+    line = ft_get_line(buffer);
+    // 删除已取出的那行，保留剩余部分
+    buffer = ft_new_buffer(buffer);
+    return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -10,9 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		GET_NEXT_LINE_H
-# define	GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-char	*get_next_line(int fd);
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+char    *get_next_line(int fd);
+char    *ft_free(char *buffer, char *buf);
+char    *ft_read(int fd, char *res);
+char    *ft_get_line(char *buffer);
+char    *ft_new_buffer(char *buffer);
+char    *ft_strjoin(const char *s1, const char *s2);
+char    *ft_strchr(const char *string, int searchedChar);
+void    ft_bzero(void *s, size_t n);
+void    *ft_calloc(size_t elementCount, size_t elementSize);
+size_t  ft_strlen(const char *theString);
 
 #endif
