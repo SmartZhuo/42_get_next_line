081490333592d54fb514ccdc6d641eb4a5b53c5a
Zhuo <119405271+SmartZhuo@users.noreply.github.com>
2025-11-23 12:24:28 +0100

    Add files via upload
    2222

--- get_next_line.c
+++ get_next_line.c
@@ -5,134 +5,76 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zhuwu <zhuwu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/26 14:11:57 by zhuwu             #+#    #+#             */
-/*   Updated: 2025/11/22 20:09:07 by zhuwu            ###   ########.fr       */
+/*   Created: 2025/11/23 11:17:15 by zhuwu             #+#    #+#             */
+/*   Updated: 2025/11/23 12:21:47 by zhuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
-char	*ft_free(char *buffer, char *buf)
-{
-	char	*temp;
-
-	temp = ft_strjoin(buffer, buf);
-	free(buffer);
-	return (temp);
-}
-
-char	*ft_new_buffer(char *buffer)
-{
-	int		i;
-	int		j;
-	char	*line;
-
-	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	if (!buffer[i])
-	{
-		free(buffer);
-		return (NULL);
-	}
-	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
-	i++;
-	j = 0;
-	while (buffer[i])
-		line[j++] = buffer[i++];
-	free(buffer);
-	return (line);
-}
-
-char	*ft_get_line(char *buffer)
-{
-	char	*line;
-	int		i;
-
-	i = 0;
-	if (!buffer[i])
-		return (NULL);
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	line = ft_calloc(i + 2, sizeof(char));
-	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
-	{
-		line[i] = buffer[i];
-		i++;
-	}
-	if (buffer[i] && buffer[i] == '\n')
-		line[i++] = '\n';
-	return (line);
-}
-
-char	*ft_read(int fd, char *res)
-{
-	char	*buffer;
-	int		flag;
-
-	if (!res)
-		res = ft_calloc(1, 1);
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	flag = 1;
-	while (flag > 0)
-	{
-		flag = read(fd, buffer, BUFFER_SIZE);
-		if (flag == -1)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		buffer[flag] = 0;
-		res = ft_free(res, buffer);
-		if (ft_strchr(buffer, '\n'))
-			break ;
-	}
-	free(buffer);
-	return (res);
-}
+# include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	char		*rest;
 	char		*line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	static char	buffer[BUFFER_SIZE + 1];
+	
+	if (fd < 0 || BUFFER_SIZE <= 0)
 	{
-		if (buffer)
-		{
-			free(buffer);
-			buffer = NULL;
-		}
+		buffer[0] = '\0';
 		return (NULL);
 	}
-	buffer = ft_read(fd, buffer);
-	if (!buffer)
+	rest = NULL;
+	if (buffer[0] != '\0')
 	{
-		buffer = NULL;
+		rest = ft_strdup(buffer);
+		if(!rest)
+			return (NULL);
+	}
+	rest = ft_read(fd, rest);
+	if(!rest)
+	{
+		buffer[0] = '\0';
 		return (NULL);
 	}
-	line = ft_get_line(buffer);
-	buffer = ft_new_buffer(buffer);
+	line = ft_get_line(&rest);
+	ft_buffer(buffer, &rest);
 	return (line);
 }
 
-# include <fcntl.h>
-# include<stdio.h>
-# include<stdlib.h>
-# include "get_next_line.h"
-
-int main()
+char	*ft_read(int fd, char *rest)
 {
-	int fd;
-	char *line;
+	char	*tmp;
+	int		flag;
 
-	fd = open("test.txt",O_RDONLY);
-	while((line = get_next_line(fd)) != NULL)
+	tmp = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!tmp)
 	{
-		printf("%s",line);
-		free(line);
+		free(rest);
+		return(NULL);
 	}
-	close(fd);
-	return (0);
+	flag = 1;
+	while(!ft_strchr(rest, '\n' && flag != 0))
+	{
+		flag = read(fd, tmp, BUFFER_SIZE);
+		if (flag < 0)
+		{
+			free(tmp);
+			free(rest);
+			return (NULL);
+		}
+		if (flag == 0)
+			break;
+		tmp[flag] = '\0';
+		rest = ft_strjoin(rest, tmp);
+		if (!rest)
+		{
+			free(tmp);
+			return (NULL);
+		}
+	}
+	free(tmp);
+	return (rest);
 }
+
+
+https://github.com/eskomo04/get_next_line/blob/master/get_next_line.c
